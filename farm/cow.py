db24570110769be8ecf0402669aaedb5e4092d36
Aybuke Altuntas <aybukealtuntas@192.168.1.19>
2026-02-13 22:50:57 +0300

    feat: finalize Cow class by adding milk attribute initialization and feed method logic

--- farm/cow.py
+++ farm/cow.py
@@ -4,9 +4,12 @@ class Cow(Animal):
 
     def __init__(self):
         super().__init__()
+        self.milk = 0
 
     def talk(self):
         return 'moo'
 
     def feed(self):
         super().feed()
+        self.milk = self.milk + 2
+        return self.energy, self.milk
